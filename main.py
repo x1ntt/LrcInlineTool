927fe9a90575578957bec016bf5b6c98f10944de
x1nt <2102084678@qq.com>
2022-08-24 23:16:17 +0800

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from ui.main_ui import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.setAcceptDrops(True)
+        self.show()
+        
+    # 启用
+    def dragEnterEvent(self, e):
+        print(e)
+        e.accept()
+        
+    def dropEvent(self, e):
+        print(e)
+    
+def main():
+    app = QApplication(sys.argv)
+    
+    main_ui = MainWindow()
+    
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
