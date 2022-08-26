8dbc7d359d529f2a87b53a6d969b647b82fa35f9
x1nt <2102084678@qq.com>
2022-08-26 17:32:17 +0800

    添加右键菜单 修复删除的bug

--- custom/musiclistwidget.py
+++ custom/musiclistwidget.py
@@ -1,6 +1,6 @@
 
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QFileDialog,QAbstractItemView
+from PyQt6.QtWidgets import QFileDialog,QAbstractItemView,QMenu
 import os
 
 support_ext = [".mp3", ".flac"]
@@ -13,10 +13,13 @@ class MusicListMan():
     def addMusic(self, url):
         ext = os.path.splitext(url)[1].lower()
         if (ext in support_ext):
-            self.music_list_widget.main_window.Log_Lw.Info("添加文件: " + url)
             filename = os.path.split(url)[1]
+            if (filename in self.music_info_map.keys()):
+                self.music_list_widget.main_window.Log_Lw.Waring("列表中已经包含文件: " + filename)
+                return
             self.music_info_map[filename] = (filename, url, 0)
             self.music_list_widget.addItem(os.path.split(url)[1])
+            self.music_list_widget.main_window.Log_Lw.Info("添加文件: " + url)
         else:
             self.music_list_widget.main_window.Log_Lw.Waring("只能添加mp3或者flac后缀的文件")
             
@@ -24,6 +27,7 @@ class MusicListMan():
         items = self.music_list_widget.findItems(filename, QtCore.Qt.MatchFlag.MatchExactly)
         for item in items:
             self.music_list_widget.takeItem(self.music_list_widget.row(item))
+            del self.music_info_map[filename]
 
 
 class MusicListWidget(QtWidgets.QListWidget):
@@ -31,7 +35,11 @@ class MusicListWidget(QtWidgets.QListWidget):
         super(MusicListWidget, self).__init__(*args, **kwargs)
         self.setAcceptDrops(True)
         self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
+        self.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.CustomContextMenu)
+        
         self.music_list_man = MusicListMan(self)
+        
+        self.customContextMenuRequested.connect(self.rightMenuShow)
     
     def dragEnterEvent(self, e):
         e.accept()
@@ -51,6 +59,12 @@ class MusicListWidget(QtWidgets.QListWidget):
         for file in files[0]:
             self.music_list_man.addMusic(file)
                 
+    def rightMenuShow(self):
+        popMenu = QMenu()
+        popMenu.addAction(QtGui.QAction(u'添加', self, triggered=self.addMusic))
+        popMenu.addAction(QtGui.QAction(u'删除', self, triggered=self.removeMusic))
+        popMenu.exec(QtGui.QCursor.pos())
+    
     def removeMusic(self):
         items = self.selectedItems()
         for item in items:
